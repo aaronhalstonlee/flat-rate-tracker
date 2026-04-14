7fb74601038af5d4d6b6f003649c14ac83dc7e59
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-04-14 13:56:28 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const V = 'v2'; // bump this on every deploy
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
